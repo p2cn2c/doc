d262e4aac4027e77864f4249c542a91f03fd9785
p2cn2c <snam.zenerate.ai@gmail.com>
2023-10-27 15:30:58 +0900

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 
@@ -18,6 +17,22 @@ export default defineConfig({
   site: "https://p2cn2c.github.io",
   base: "p2cn2c/",
   // build: {
+  //   inlineStylesheets: true,
   //   assets: "p2cn2c/",
+  // },
+  // Resolves to the "./foo" directory, relative to this config file
+  root: new URL("./src", import.meta.url).toString(),
+  // Resolves to the "./public" directory, relative to this config file
+  publicDir: new URL("./public", import.meta.url).toString(),
+  // vite: {
+  //   build: {
+  //     rollupOptions: {
+  //       output: {
+  //         entryFileNames: "entry.[hash].js",
+  //         chunkFileNames: "chunks/chunk.[hash].js",
+  //         assetFileNames: "assets/asset.[hash][extname]",
+  //       },
+  //     },
+  //   },
   // },
 });
