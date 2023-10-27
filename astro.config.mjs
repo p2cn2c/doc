c32854eb1af7e4a5f7f64a2bebb1017086af5c19
p2cn2c <snam.zenerate.ai@gmail.com>
2023-10-27 12:32:44 +0900

    Feat: workflow setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 
@@ -7,6 +8,16 @@ export default defineConfig({
   integrations: [
     // Enable Preact to support Preact JSX components.
     vue(),
-    tailwind(),
+    tailwind({
+      // Example: Provide a custom path to a Tailwind config file
+      // configFile: "./tailwind-config.mjs",
+      // applyBaseStyles: false,
+    }),
+    // image(),
   ],
+  site: "https://p2cn2c.github.io",
+  base: "p2cn2c/",
+  // build: {
+  //   assets: "p2cn2c/",
+  // },
 });
--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const SITE = {
-  title: "Astro Design System",
-  description: "Astro theme to build your design system fast.",
-  defaultLanguage: "en_US",
-  twitter: "@jordienr",
-  github: "jordienr",
-  linkedin: "linkedinUsername",
+  title: "Doc",
+  description: "whewchews wiki",
+  defaultLanguage: "ko_KR",
+  twitter: "",
+  github: "p2cn2c",
+  linkedin: "",
 };
 
 export const OPEN_GRAPH = {
@@ -15,23 +15,25 @@ export const OPEN_GRAPH = {
   twitter: "",
 };
 
+export const DEFAULT_URL = "/p2cn2c";
+
 export const SIDEBAR = [
   { text: "react.dev", header: true },
-  { text: "Introduction", link: "/react/introduction" },
-  { text: "Describing the UI", link: "/react/describing-the-ui" },
+  { text: "Introduction", link: `${DEFAULT_URL}/react/introduction` },
+  { text: "Describing the UI", link: `${DEFAULT_URL}/react/describing-the-ui` },
 
   { text: "Core", header: true },
-  { text: "Introduction", link: "/core/introduction" },
-  { text: "Colors", link: "/core/colors" },
-  { text: "Typography", link: "/core/typography" },
-  { text: "Shadows", link: "/core/shadows" },
+  { text: "Introduction", link: `${DEFAULT_URL}/core/introduction` },
+  { text: "Colors", link: `${DEFAULT_URL}/core/colors` },
+  { text: "Typography", link: `${DEFAULT_URL}/core/typography` },
+  { text: "Shadows", link: `${DEFAULT_URL}/core/shadows` },
 
   { text: "Components", header: true },
-  { text: "Buttons", link: "/components/buttons" },
-  { text: "Input", link: "/components/input" },
-  { text: "Status pills", link: "/components/status-pill" },
-  { text: "Table", link: "/components/table" },
+  { text: "Buttons", link: `${DEFAULT_URL}/components/buttons` },
+  { text: "Input", link: `${DEFAULT_URL}/components/input` },
+  { text: "Status pills", link: `${DEFAULT_URL}/components/status-pill` },
+  { text: "Table", link: `${DEFAULT_URL}/components/table` },
 
   { text: "Patterns", header: true },
-  { text: "Introduction", link: "/patterns/introduction" },
+  { text: "Introduction", link: `${DEFAULT_URL}/patterns/introduction` },
 ];
