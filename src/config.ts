adcdb3bf6166e9813ab3673ca83e98148a65486f
whewchews <whewchews@gmail.com>
2023-11-01 03:32:40 +0900

    Docs: React CH1 Describing the UI

--- src/config.ts
+++ src/config.ts
@@ -18,22 +18,22 @@ export const OPEN_GRAPH = {
 export const DEFAULT_URL = "/p2cn2c";
 
 export const SIDEBAR = [
-  { text: "react.dev", header: true },
+  { text: "React.dev", header: true },
   { text: "Introduction", link: `${DEFAULT_URL}/react/introduction` },
   { text: "Describing the UI", link: `${DEFAULT_URL}/react/describing-the-ui` },
 
-  { text: "Core", header: true },
-  { text: "Introduction", link: `${DEFAULT_URL}/core/introduction` },
-  { text: "Colors", link: `${DEFAULT_URL}/core/colors` },
-  { text: "Typography", link: `${DEFAULT_URL}/core/typography` },
-  { text: "Shadows", link: `${DEFAULT_URL}/core/shadows` },
+  // { text: "Core", header: true },
+  // { text: "Introduction", link: `${DEFAULT_URL}/core/introduction` },
+  // { text: "Colors", link: `${DEFAULT_URL}/core/colors` },
+  // { text: "Typography", link: `${DEFAULT_URL}/core/typography` },
+  // { text: "Shadows", link: `${DEFAULT_URL}/core/shadows` },
 
-  { text: "Components", header: true },
-  { text: "Buttons", link: `${DEFAULT_URL}/components/buttons` },
-  { text: "Input", link: `${DEFAULT_URL}/components/input` },
-  { text: "Status pills", link: `${DEFAULT_URL}/components/status-pill` },
-  { text: "Table", link: `${DEFAULT_URL}/components/table` },
+  // { text: "Components", header: true },
+  // { text: "Buttons", link: `${DEFAULT_URL}/components/buttons` },
+  // { text: "Input", link: `${DEFAULT_URL}/components/input` },
+  // { text: "Status pills", link: `${DEFAULT_URL}/components/status-pill` },
+  // { text: "Table", link: `${DEFAULT_URL}/components/table` },
 
-  { text: "Patterns", header: true },
-  { text: "Introduction", link: `${DEFAULT_URL}/patterns/introduction` },
+  // { text: "Patterns", header: true },
+  // { text: "Introduction", link: `${DEFAULT_URL}/patterns/introduction` },
 ];
--- src/pages/react/introduction.md
+++ src/pages/react/introduction.md
@@ -4,6 +4,8 @@ layout: ../../layouts/MainLayout.astro
 
 # Introduction
 
-Docs: [React.dev](https://react.dev)
+리액트 공식문서 학습
+
+Docs: [React.dev](https://react.dev/learn/')
 
 - 2023.10.27 ~ 2023.12.20 (예정)
